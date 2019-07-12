251b0c9d15850dfb1a469d84dab45108272cb0d2
Mike Boyd <mike.boyd@radicalbear.com>
2019-07-12 18:34:24 -0400

    Add routing tests

--- spec/routing/movies_routing_spec.rb
+++ spec/routing/movies_routing_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 RSpec.describe MoviesController, type: :routing do
   describe "routing" do
+    it "routes root to #index" do
+      expect(:get => "/").to route_to("movies#index")
+    end
+
     it "routes to #index" do
       expect(:get => "/movies").to route_to("movies#index")
     end
