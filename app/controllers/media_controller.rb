78d5ede65a694b72ac2346b44eded3f19198fdca
Mike Boyd <mike.boyd@radicalbear.com>
2019-07-28 17:57:15 -0400

    Remove json controller scaffold and views

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -29,10 +29,8 @@ class MediaController < ApplicationController
     respond_to do |format|
       if @medium.save
         format.html { redirect_to @medium, notice: 'Medium was successfully created.' }
-        format.json { render :show, status: :created, location: @medium }
       else
         format.html { render :new }
-        format.json { render json: @medium.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -41,10 +39,8 @@ class MediaController < ApplicationController
     respond_to do |format|
       if @medium.update(medium_params)
         format.html { redirect_to @medium, notice: 'Medium was successfully updated.' }
-        format.json { render :show, status: :ok, location: @medium }
       else
         format.html { render :edit }
-        format.json { render json: @medium.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,7 +49,6 @@ class MediaController < ApplicationController
     @medium.destroy
     respond_to do |format|
       format.html { redirect_to media_url, notice: 'Medium was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/views/media/_medium.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! medium, :id, :title, :suggested_by, :rating, :created_at, :updated_at
-json.url medium_url(medium, format: :json)
