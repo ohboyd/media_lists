c329922f8ad6e31e3021332208bf9ca267baf014
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-07 21:15:27 -0400

    Create book seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,11 @@ Medium.create!(title: 'Ghost in the Shell', suggested_by: 'Jide', media_type: 'm
 Medium.create!(title: 'Cowboy Bebop', suggested_by: 'Alex', media_type: 'movie')
 Medium.create!(title: 'Midsommar', suggested_by: 'Eldred', media_type: 'movie')
 Medium.create!(title: 'Us', suggested_by: 'Mars', media_type: 'movie')
+
+# TODO: run these in the heroku console to make sure they're on prod
+Medium.create!(title: 'Dune', suggested_by: 'Sirlich', media_type: 'book')
+Medium.create!(title: 'Agile Software Development', suggested_by: 'Jim', media_type: 'book')
+Medium.create!(title: 'Object Oriented Construction', suggested_by: 'Weirich', media_type: 'book')
+Medium.create!(title: 'The Pragmatic Programmer', suggested_by: 'David', media_type: 'book')
+
+# TODO: add factory bot and faker so I can randomly create all this test data for local usage 
