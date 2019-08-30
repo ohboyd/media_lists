bde61d4383c06511a9c79066ba947e51f812aa25
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-29 22:49:40 -0400

    Create validations for the reviews table

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :media
+
+  validates :comment, length: { minimum: 4 }
+
+  STARS = [1, 2, 3, 4, 5]
+  validates :stars, inclusion: { in: STARS, message: 'Must be between 1 and 5' }
 end
