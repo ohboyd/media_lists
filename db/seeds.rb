729c261a04cc4595ed76c3177795783b176b1319
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-31 19:06:15 -0400

    Update seeds to have single type of each media for reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
 puts 'seeding media'
 
 if Medium.count.zero?
-  15.times { FactoryBot.create :movie }
-  15.times { FactoryBot.create :book }
-  15.times { FactoryBot.create :show }
-  15.times { FactoryBot.create :podcast }
-  15.times { FactoryBot.create :music }
+  10.times { FactoryBot.create :movie }
+  10.times { FactoryBot.create :book }
+  10.times { FactoryBot.create :show }
+  10.times { FactoryBot.create :podcast }
+  10.times { FactoryBot.create :music }
 end
 
 puts 'seeding reviews'
-movie = Medium.find(1)
-
 if Review.count.zero?
-  1.upto(10) do
-    FactoryBot.create :review
-    # Review.create!(stars:4, comment: Faker::Hipster.paragraph, medium_id: (n * 10))
+  1.upto(5) do |n|
+    FactoryBot.create :review, medium_id: (n * 10)
+    # Review.create!(stars:4, comment: Faker::Hipster.paragraph, medium_id: n * 10, pick: true)
   end
 end
