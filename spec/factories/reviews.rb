9b9005ebff2494673e401e2f1cf03edb4ed6ee58
Mike Boyd <mike.boyd@radicalbear.com>
2019-09-02 18:46:50 -0400

    Update model specs for reviews

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -2,7 +2,11 @@ FactoryBot.define do
   factory :review do
     comment { Faker::Hipster.paragraph }
     stars { rand(1..5) }
-    pick { true }
     association :medium, factory: :movie
+    # should be able to just use movie without association :medium, factory: :movie. Need to make sure that works with seeding and rspec tests before making the transition permanent.
+
+    factory :picked_review do
+      pick { true }
+    end
   end
 end
---
+++ spec/models/review_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  let(:movie) { create :movie }
+  let(:movie_two) { create :movie }
+  subject { described_class.new(comment: 'super interesting', stars: 3, medium_id: movie.id)}
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a comment' do
+      subject.comment = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid when stars are outside range 1..5' do
+      subject.stars = 6
+      expect(subject).not_to be_valid
+    end
+  end
+
+  describe 'scopes' do
+    let!(:review) { create :review, medium_id: movie.id }
+    let!(:picked_review) { create :picked_review, medium_id: movie_two.id }
+
+    context '#picks' do
+      it 'returns reviews that have been chosen as picks' do
+        expect(Review.picks).to include(picked_review)
+      end
+
+      it 'excludes un-picked reviews' do
+        expect(Review.picks).not_to include(review)
+      end
+    end
+
+    context '#this_month' do
+      it 'selects reviews created within the current month' do
+        expect(Review.this_month).to include(review && picked_review)
+      end
+
+      it 'excludes reviews created in other months' do
+        review.created_at = Time.now - 2.months
+        picked_review = Time.now - 1.year
+        expect(Review.this_month).not_to include(review && picked_review)
+      end
+    end
+  end
+
+  describe 'methods' do
+    context '#stars_as_percent' do
+      it 'calculates the percentage of stars from an integer' do
+        subject.stars = 4.5
+        expect(subject.stars_as_percent).to eq 80.0
+        subject.stars = 2
+        expect(subject.stars_as_percent).to eq 40.0
+      end
+    end
+  end
+end
