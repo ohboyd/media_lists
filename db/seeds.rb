88aa909ec161b331a1679c9c77b1e632c0a977da
Mike Boyd <mike.boyd@radicalbear.com>
2019-09-08 20:46:23 -0400

    Update seeds to have reviews across multiple months

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,14 @@ end
 puts 'seeding reviews'
 if Review.count.zero?
   1.upto(5) do |n|
-    FactoryBot.create :review, medium_id: (n * 10)
+    FactoryBot.create :review, created_at: Time.now.last_month, medium_id: (n * 10), pick: true
     # Review.create!(stars:4, comment: Faker::Hipster.paragraph, medium_id: n * 10, pick: true)
   end
+
+  puts 'creating additional movie reviews for this month'
+  FactoryBot.create :movie
+  FactoryBot.create :review, medium_id: Medium.last.id, pick: true
+
+  FactoryBot.create :movie
+  FactoryBot.create :review, medium_id: Medium.last.id, pick: true
 end
