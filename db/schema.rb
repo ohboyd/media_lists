5ee6502dfc3ae30cbe17bf775fbe5129d4097032
Mike Boyd <mike.boyd@radicalbear.com>
2019-09-12 17:18:45 -0400

    Update schema for this branch

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_061918) do
+ActiveRecord::Schema.define(version: 2019_09_12_063222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,17 +43,17 @@ ActiveRecord::Schema.define(version: 2019_09_12_061918) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "position"
-    t.integer "media_type", null: false
+    t.integer "media_type"
     t.string "reason_for_suggestion"
     t.string "link"
     t.index ["media_type"], name: "index_media_on_media_type"
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.text "comment", null: false
-    t.integer "stars", null: false
+    t.text "comment"
+    t.integer "stars"
     t.boolean "pick", default: false, null: false
-    t.bigint "medium_id", null: false
+    t.bigint "medium_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["medium_id"], name: "index_reviews_on_medium_id"
