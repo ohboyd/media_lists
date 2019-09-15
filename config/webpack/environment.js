1b64011c4b2b1ef180a304571a1957e7d1a502a9
Mike Boyd <mike.boyd@radicalbear.com>
2019-09-15 12:58:02 -0400

    Try prepend instead of append

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.append('Provide',
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
