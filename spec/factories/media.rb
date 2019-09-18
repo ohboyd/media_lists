b213a5e0dc734dc3bf59fed855a4662e032c0028
Mike Boyd <mike.boyd@radicalbear.com>
2019-09-17 20:07:12 -0400

    Add virtual attribute to factories

--- spec/factories/media.rb
+++ spec/factories/media.rb
@@ -5,35 +5,35 @@ FactoryBot.define do
       title { Faker::Book.title }
       suggested_by { Faker::GreekPhilosophers.name }
       media_type { 'movie' }
-      reason_for_suggestion { Faker::TvShows::SiliconValley.motto }
+      suggestion_reason { Faker::TvShows::SiliconValley.motto }
     end
 
     factory :book do
       title { Faker::Book.title }
       suggested_by { Faker::Book.author }
       media_type { 'book' }
-      reason_for_suggestion { Faker::Quote.famous_last_words }
+      suggestion_reason { Faker::Quote.famous_last_words }
     end
 
     factory :show do
       title { Faker::TvShows::BreakingBad.episode }
       suggested_by { Faker::TvShows::RickAndMorty.character }
       media_type { 'show' }
-      reason_for_suggestion { Faker::TvShows::RickAndMorty.quote }
+      suggestion_reason { Faker::TvShows::RickAndMorty.quote }
     end
 
     factory :podcast do
       title { Faker::Hipster.sentence(word_count: 3) }
       suggested_by { Faker::Science.scientist }
       media_type { 'podcast' }
-      reason_for_suggestion { Faker::TvShows::BojackHorseman.tongue_twister }
+      suggestion_reason { Faker::TvShows::BojackHorseman.tongue_twister }
     end
 
     factory :music do
       title { Faker::Music.band }
       suggested_by { Faker::Music::RockBand.name }
       media_type { 'music' }
-      reason_for_suggestion { Faker::Quote.yoda }
+      suggestion_reason { Faker::Quote.yoda }
     end
   end
 end
