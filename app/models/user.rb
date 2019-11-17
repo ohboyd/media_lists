33dbdf74f766ee516bc49e3dc0aca167ae38faa9
Mike Boyd <mike.boyd@radicalbear.com>
2019-11-17 15:49:14 -0500

    Change password requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 10, allow_blank: true }
+  validates :password, length: { minimum: 7, allow_blank: true }
 end
