a9f162c1731a9de47877569f10f515d573405954
Mike Boyd <mike.boyd@radicalbear.com>
2019-11-24 16:58:33 -0500

    Add require signin method to force user to signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  private
+
+    def require_signin
+      unless current_user
+        session[:intended_url] = request.url
+        redirect_to signin_path, alert: "Please sign in first!"
+      end
+    end
 end
